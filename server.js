c0e24ff09b42663af02fe5e0de6c20a5c9be1ddd
hanyang <513915503@qq.com>
2016-12-19 17:08:45 +0800

    解决刷新404问题

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const path = require("path")
 const express = require("express")
 const app = express()
+const contentBase = path.join(__dirname, "dist")
 
 app.set("x-powered-by", false)
 
-app.use(express.static(path.join(__dirname, "dist")))
-app.use("/static/cnode/", express.static(path.join(__dirname, "dist")))
+app.use(express.static(contentBase))
+app.use("/static/cnode/", express.static(contentBase))
 
 app.get("*", (req, res) => {
-	res.sendFile(path.resolve(__dirname, "./index.html"))
+	res.sendFile(path.join(__dirname, "dicontentBase/index.html"))
 })
 
 app.listen(8080)
